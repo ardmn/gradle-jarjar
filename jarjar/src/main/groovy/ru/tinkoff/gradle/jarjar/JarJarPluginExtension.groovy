a7cc7603c3144dbb1609c19587571d708bad65c4
ardmn <artiom999@gmail.com>
2018-04-02 00:36:18 +0300

    - test

--- jarjar/src/main/groovy/ru/tinkoff/gradle/jarjar/JarJarPluginExtension.groovy
+++ jarjar/src/main/groovy/ru/tinkoff/gradle/jarjar/JarJarPluginExtension.groovy
@@ -5,7 +5,7 @@ package ru.tinkoff.gradle.jarjar
  */
 class JarJarPluginExtension {
 
-    String jarJarDependency = 'com.ardmn:jarjar:1.0.5'
+    String jarJarDependency = 'com.ardmn:jarjar:1.0.6'
 
     Map<String, String> rules = [:]
 
